c72c16cf3bc975907bbf8a75ef2c6a58e3d88e4e
MaxDev6966 <85440084+MaxGamingOfficalYT@users.noreply.github.com>
2022-11-26 17:47:18 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "BOT_TOKEN",
+    channel: "DISCORD_CHANNEL_ID",
+    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
+    channel_id: "YOUTUBE_ID",
+    watchInterval: 30000
+}
